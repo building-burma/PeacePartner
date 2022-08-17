b3d75f26590b91e0cf7579d6b22d66efde0e01ec
MidasVanVeen <midasvanveen.email@gmail.com>
2022-08-17 22:25:08 +0200

    fixed typo

--- server.js
+++ server.js
@@ -41,7 +41,7 @@ app.use(upload.array()) // for parsing application/x-www-form-urlencoded
 // logging of requests happens here. all requests first pass through here. banning/blacklisting of ips
 // should also happen here.
 app.all("*",(req,res,next) => {
-    logger(`Recieved request from ${req.ip} for ${req.path}`)
+    logger(`Received request from ${req.ip} for ${req.path}`)
     next();
 });
 
