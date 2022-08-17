f6ed060c7120699725caa48c97e98aadd7179729
MidasVanVeen <midasvanveen.email@gmail.com>
2022-08-17 21:26:36 +0200

    added form switcher code

--- static/js/registryValidator.js
+++ static/js/registryValidator.js
@@ -1,1 +1,22 @@
 const form = document.getElementById("register_form");
+
+window.onload = () => {
+    form.onchange = (e) => {
+        if (form.elements['type'].value === "SP") {
+            Array.from(document.getElementsByClassName("sponsor")).forEach((i) => {
+                i.style.display = "inline-block";
+            })
+            Array.from(document.getElementsByClassName("refugee")).forEach((i) => {
+                i.style.display = "none";
+            })
+        }
+        if (form.elements['type'].value === "RU") {
+            Array.from(document.getElementsByClassName("refugee")).forEach((i) => {
+                i.style.display = "inline-block";
+            })
+            Array.from(document.getElementsByClassName("sponsor")).forEach((i) => {
+                i.style.display = "none";
+            })
+        }
+    }
+}
