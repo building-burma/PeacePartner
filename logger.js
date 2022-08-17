92883940801e8737943ca6e82d9dd232ffb3b249
MidasVanVeen <midasvanveen.email@gmail.com>
2022-08-17 22:22:08 +0200

    added logger

---
+++ logger.js
@@ -0,0 +1,10 @@
+const fs = require("fs");
+
+// datetime of execution for logging
+const startdate = new Date();
+
+module.exports = function logger(string) {
+    fs.appendFile(__dirname + `/logs/${startdate}.log`, string + "\n", (err) => {
+        console.error("Error logging");
+    });
+}
