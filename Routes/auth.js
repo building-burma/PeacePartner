cf307bea8947cf3a7b5380b35e7bf850eee19f7c
MidasVanVeen <midasvanveen.email@gmail.com>
2022-08-27 22:12:57 +0200

    changed database access from callback to async-await

--- Routes/auth.js
+++ Routes/auth.js
@@ -24,30 +24,23 @@ router.get("/login.html", (req,res,next) => {
 });
 
 router.post("/login.html", (req,res,next) => {
-    if (req.body.username && req.body.password) {
-        db.connect((err, client, done) => {
-            client.query('SELECT passhash, passsalt FROM users WHERE username = $1;', [req.body.username], (err, dbres) => {
-                done()
-                if (err) {
-                    console.log(err.stack)
-                    // TODO: handle err
-                } else {
-                    console.log(dbres.rows[0])
-                    if (crypto.createHash('sha256').update(req.body.password + dbres.rows[0].passsalt).digest('hex') === dbres.rows[0].passhash) {
-                        req.session.loggedIn = true;
-                        req.session.username = req.body.username; 
+    // using an async IIFE
+    ;(async () => {
+        const client = await db.connect();
+        const result = await client.query('SELECT * FROM users WHERE username = $1', [req.body.username]);
+        if (crypto.createHash('sha256').update(req.body.password + result.rows[0].passsalt).digest('hex') === result.rows[0].passhash) {
+            req.session.loggedIn = true;
+            req.session.username = req.body.username;
+            req.session.userid = result.rows[0].id;
 
-                        res.statusCode = 302
-                        res.setHeader('Location', '/home.html')
-                        res.write('Redirecting')
-                        res.end()
-                    } else {
-                        res.redirect("/login.html");
-                    }
-                }
-            })
-        })
-    }
+            res.statusCode = 302
+            res.setHeader('Location', '/home.html')
+            res.write('Redirecting')
+            res.end()
+        } else {
+            res.redirect("/login.html");
+        }
+    })()
 });
 
 router.get("/register.html", (req,res,next) => {
