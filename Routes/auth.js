00726250908f046095b5e586d3b9e9f971c1fe00
MidasVanVeen <midasvanveen.email@gmail.com>
2022-08-27 22:15:59 +0200

    reformed most of the registry. * changed to async-await * added retIfMissing * added email and pets fields

--- Routes/auth.js
+++ Routes/auth.js
@@ -55,35 +55,56 @@ router.get("/register.html", (req,res,next) => {
     });
 });
 
-router.post("/register.html", (req,res,next) => {
+router.post("/register.html", upload.single("picture"),(req,res,next) => {
     const i = req.body // i for inputs. code is way cleaner like this :0
 
     const validGeneralInputs = {
         type: i.type,
         name: i.name, // TODO: clean input
         accomodatenum: (Number(i.accommodatenum) >= 0) ? Number(i.accommodatenum) : undefined,
+        pets: (i.pets > 0) ? i.pets: undefined,
         username: i.username, // TODO: clean input
         passhash: (i.passhash.length === 64 && i.passhash.match(/[0-9a-f]+/)[0] === i.passhash) ? i.passhash : undefined,
         passsalt: (i.passsalt.length === 128 && i.passsalt.match(/[0-9A-Za-z]+/)[0] === i.passsalt) ? i.passsalt : undefined,
+        email: i.email,
     }
     
     const requiredGeneralInputs = [
         'type',
         'name',
         'accomodatenum',
+        'pets',
         'username',
         'passhash',
         'passsalt',
+        'email',
     ];
 
-    const invalidError = () => {
-        res.send("Invalid input");
-        res.end()
+    let missing = [];
+
+    const retIfMissing = () => {
+        if (missing.length !== 0) {
+            res.redirect(`/register.html?missing=${missing}`);
+            console.log(`/register.html?missing=${missing[0]}`)
+            console.log('redirecting')
+            res.end()
+            return true;
+        }
+        return false
     }
 
-    for (let n=0;n<requiredGeneralInputs.length;n++) {
-        if (!validGeneralInputs[n]) return invalidError();
+    let pettypes = "";
+    for (let n=0;n<validGeneralInputs.pets;n++) {
+        pettypes += i['pet'+n] + ',';
     }
+    validGeneralInputs.pettypes = pettypes;
+
+    requiredGeneralInputs.forEach((n) => {
+        if (!validGeneralInputs[n]) {
+            missing.push(n);
+        }
+    });
+    if (retIfMissing()) return;
 
     let foreignid;
 
@@ -92,7 +113,7 @@ router.post("/register.html", (req,res,next) => {
         const validSponsorInputs = {
             familycomp: (i.familycomp === "SM" || i.familycomp === "SF" || i.familycomp === "FA") ? i.familycomp : undefined,
             country: (i.country === "US" || i.country ===  "CA" || i.country === "GB") ? i.country : undefined,
-            city: i.city,
+            city: (i.city !== "") ? i.city : undefined,
             state: i.state,
         }
 
@@ -101,48 +122,49 @@ router.post("/register.html", (req,res,next) => {
             'country',
             'city',
         ];
-        
-        for (let n=0;n<requiredSponsorInputs.length;n++) {
-            if (!validSponsorInputs[n]) return invalidError();
-        }
+
+        requiredSponsorInputs.forEach((n) => {
+            if (!validSponsorInputs[n]) {
+                missing.push(n);
+            }
+        });
+        if (retIfMissing()) return;
 
         // create sponsors table entry
         // * get id of last sponsor entry
         // * create entry and save id for users table
-        db.connect((err, client, done) => {
-            if (!err) {
-                client.query('SELECT id FROM sponsors ORDER BY id DESC LIMIT 1',(iderr, idres) => {
-                    if (!iderr) {
-                        foreignid = idres.rows[0].id + 1; // TODO: validate if this is a number > 0. if nothing, id = 1
-                        client.query('INSERT INTO sponsors (id, familycomp, accommodatenum, country, city, state) VALUES ($1,$2,$3,$4,$5,$6);',
-                            [foreignid,
-                                validSponsorInputs.familycomp,
-                                validGeneralInputs.accomodatenum,
-                                validSponsorInputs.country,
-                                validSponsorInputs.city,
-                                validGeneralInputs.state
-                            ], (inserterr) => {
-                                if (!inserterr) {
-                                    createUser();
-                                }
-                                done();
-                            });
-                    }
-                });
-            }
-        })
+
+        // using async IIFE
+        ;(async () => {
+            const client = await db.connect();
+            const sponsorIdRes = await client.query('SELECT id FROM sponsors ORDER BY id DESC LIMIT 1');
+            foreignid = (sponsorIdRes.rows[0]) ? sponsorIdRes.rows[0].id + 1 : 1;
+            console.log('refugee')
+            await client.query('INSERT INTO sponsors (id, familycomp, accommodatenum, pets, pettypes, country, city, state) VALUES ($1,$2,$3,$4,$5,$6,$7,$8);',
+                [foreignid,
+                validSponsorInputs.familycomp,
+                validGeneralInputs.accomodatenum,
+                validGeneralInputs.pets,
+                (validGeneralInputs.pettypes !== "") ? validGeneralInputs.pettypes : null,
+                validSponsorInputs.country,
+                validSponsorInputs.city,
+                validGeneralInputs.state]
+            );
+            createUser(validSponsorInputs);
+            client.release()
+        })()
     }
 
     if (validGeneralInputs.type === "RU") {
         // verify refugee specific input
-        const validRefugeeInputs = {
+
+        let validRefugeeInputs = {
             currcity: i.currcity,
             currcountry: i.currcountry,
-            prefcountry: (i.country === "US" || i.country ===  "CA" || i.country === "GB") ? i.country : undefined,
-            prefcity: i.city,
+            prefcountry: (i.prefcountry === "US" || i.prefcountry ===  "CA" || i.prefcountry === "GB") ? i.prefcountry : undefined,
+            prefcity: i.prefcity,
             state: i.state,
-            pets: i.pets,
-            picture: i.picture,
+            picture: (req.file.mimetype === "image/png") ? req.file : undefined,
             special: i.special
         }
 
@@ -151,66 +173,73 @@ router.post("/register.html", (req,res,next) => {
             'prefcity',
             'currcountry',
             'prefcountry',
-            'pets',
             'picture',
         ];
 
-        for (let n=0;n<requiredRefugeeInputs.length;n++) {
-            if (!validRefugeeInputs[n]) return invalidError();
-        }
+        requiredRefugeeInputs.forEach((n) => {
+            if (!validRefugeeInputs[n]) {
+                missing.push(n);
+            }
+        });
+        if (retIfMissing()) return;
 
         // create sponsors table entry
         // * get id of last sponsor entry
         // * create entry and save id for users table
-        db.connect((err, client, done) => {
-            if (!err) {
-                client.query('SELECT id FROM refugees ORDER BY id DESC LIMIT 1',(iderr, idres) => {
-                    if (!iderr) {
-                        foreignid = idres.rows[0].id + 1; // TODO: validate if this is a number > 0. if nothing, id = 1
-                        client.query('INSERT INTO refugees (id, currcity, currcountry, prefcity, prefcountry, prefstate, persons, pets, picture) VALUES ($1,$2,$3,$4,$5,$6);',
-                            [foreignid,
-                                validRefugeeInputs.currcity,
-                                validRefugeeInputs.currcountry,
-                                validRefugeeInputs.prefcity,
-                                validRefugeeInputs.prefcountry,
-                                validGeneralInputs.state,
-                                validGeneralInputs.accomodatenum,
-                                validRefugeeInputs.pets,
-                                validRefugeeInputs.picture,
-                            ], (inserterr) => {
-                                if (!inserterr) {
-                                    createUser();
-                                }
-                                done();
-                            });
-                    }
-                });
-            }
-        })
-
+        // using async IIFE
+        ;(async () => {
+            const client = await db.connect();
+            const refugeeIdRes = await client.query('SELECT id FROM refugees ORDER BY id DESC LIMIT 1');
+            foreignid = (refugeeIdRes.rows[0]) ? refugeeIdRes.rows[0].id + 1 : 1;
+            await client.query('INSERT INTO refugees (id, currcity, currcountry, prefcity, prefcountry, prefstate, persons, pets, pettypes, picture) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10);',
+                [foreignid,
+                validRefugeeInputs.currcity,
+                validRefugeeInputs.currcountry,
+                validRefugeeInputs.prefcity,
+                validRefugeeInputs.prefcountry,
+                validGeneralInputs.state,
+                validGeneralInputs.accomodatenum,
+                validGeneralInputs.pets,
+                (validGeneralInputs.pettypes !== "") ? validGeneralInputs.pettypes : null,
+                validRefugeeInputs.picture.buffer,]
+            );
+            createUser(validRefugeeInputs);
+            client.release()
+        })()
     }
 
-    const createUser = () => {
-        db.connect((err, client, done) => {
-            if (!err) {
-                client.query('SELECT id FROM users ORDER BY id DESC LIMIT 1', (iderr,idres) => {
-                    const userid = idres.rows[0].id + 1; //TODO: you know.
-                    const foreignidName = (type === "SP") ? "sponsorId" : "refugeeId";
-                    client.query(`INSERT INTO users (id, type, name, username, passhash, passsalt, ${foreignidName}) VALUES ($1,$2,$3,$4,$5,$6,$7);`,
-                        [
-                            userid,
-                            (validGeneralInputs.type === "SP"),
-                            validGeneralInputs.name,
-                            validGeneralInputs.username,
-                            validGeneralInputs.passhash,
-                            validGeneralInputs.passsalt,
-                            foreignid
-                        ], (inserterr) => {
-                        res.send("success")
-                    })
-                })
+    // TODO: check if username exists
+    const createUser = (extraInputs) => {
+        ;(async () => {
+            const client = await db.connect();
+            const usernameCheck = await client.query('SELECT id FROM users WHERE username = $1',[validGeneralInputs.username]);
+            if (usernameCheck.rows[0]) {
+                console.log('username exists');
+                // TODO: add invalid username behaviour
+                res.redirect('/register.html?usernameerr=1');
+                // TODO: delete sponsor/refugee entry
+                return
             }
-        });
+            const idres = await client.query('SELECT id FROM users ORDER BY id DESC LIMIT 1');
+            const userid = (idres.rows[0]) ? idres.rows[0].id + 1 : 1; //TODO: you know.
+            const foreignidName = (validGeneralInputs.type === "SP") ? "sponsorId" : "refugeeId";
+            console.log('create user')
+            await client.query(`INSERT INTO users (id, type, name, username, email, passhash, passsalt, ${foreignidName}) VALUES ($1,$2,$3,$4,$5,$6,$7,$8);`,
+                [userid,
+                (validGeneralInputs.type === "SP"),
+                validGeneralInputs.name,
+                validGeneralInputs.username,
+                validGeneralInputs.email,
+                validGeneralInputs.passhash,
+                validGeneralInputs.passsalt,
+                foreignid]
+            );
+            res.statusCode = 302
+            res.setHeader('Location', '/home.html')
+            res.write('Redirecting')
+            res.end()
+            matcher(Object.assign({id:userid}, validGeneralInputs, extraInputs));
+        })()
     }
 })
 
